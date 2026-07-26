1e5e78c845480c03ab71bed52aa4fc340c8e8b25
starfish <starfish.hu@gmail.com>
2026-07-27 00:50:19 +0800

    chore(fs): ignore fuzz build outputs

--- fuzz/target/release/deps/shlex-987dfdf79c516297.d
+++
@@ -1,8 +0,0 @@
-/home/starfish/working/qubit/rust-common/rs-fs/fuzz/target/release/deps/shlex-987dfdf79c516297.d: /home/starfish/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-2.0.1/src/lib.rs /home/starfish/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-2.0.1/src/bytes.rs
-
-/home/starfish/working/qubit/rust-common/rs-fs/fuzz/target/release/deps/libshlex-987dfdf79c516297.rlib: /home/starfish/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-2.0.1/src/lib.rs /home/starfish/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-2.0.1/src/bytes.rs
-
-/home/starfish/working/qubit/rust-common/rs-fs/fuzz/target/release/deps/libshlex-987dfdf79c516297.rmeta: /home/starfish/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-2.0.1/src/lib.rs /home/starfish/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-2.0.1/src/bytes.rs
-
-/home/starfish/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-2.0.1/src/lib.rs:
-/home/starfish/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-2.0.1/src/bytes.rs:
--- fuzz/target/x86_64-unknown-linux-gnu/release/deps/path_uri-e05a373cde7e8985.d
+++
@@ -1,5 +0,0 @@
-/home/starfish/working/qubit/rust-common/rs-fs/fuzz/target/x86_64-unknown-linux-gnu/release/deps/path_uri-e05a373cde7e8985.d: fuzz_targets/path_uri.rs
-
-/home/starfish/working/qubit/rust-common/rs-fs/fuzz/target/x86_64-unknown-linux-gnu/release/deps/path_uri-e05a373cde7e8985: fuzz_targets/path_uri.rs
-
-fuzz_targets/path_uri.rs:
